8d02a3205285b1bdbf68693159cbcf79334faa9b
Jalingpp <jpjia22@stu.ecnu.edu.cn>
2024-07-17 11:17:39 +0000

    test new branch

--- run.sh
+++ run.sh
@@ -4,14 +4,14 @@ else
     round=$1
 fi
 
-# bash test_batchSize.sh $round
-# sleep 1
-# bash test_mehtBC.sh $round
-# sleep 1
-# bash test_mehtBS.sh $round
-# sleep 1
-# bash test_mbtBN.sh $round
-# sleep 1
+bash test_batchSize.sh $round
+sleep 1
+bash test_mehtBC.sh $round
+sleep 1
+bash test_mehtBS.sh $round
+sleep 1
+bash test_mbtBN.sh $round
+sleep 1
 bash test_thread.sh $round
 sleep 1
 bash test_scale.sh $round
